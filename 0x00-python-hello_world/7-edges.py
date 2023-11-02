fb0df1dddc9919eba9a5e8ccae68e1af2cdc9267
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-11-02 03:03:03 +0200

    hkhyyyjyg

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first = word[:3]
+second = word[7:]
+third = word[1:7]
+print("First 3 letters: " + first)
+print("Last 2 letters: " + second)
+print("Middle word: " + third)
