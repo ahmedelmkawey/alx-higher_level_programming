7f9505fa579788b8bd4a21f74da85368284bbc1b
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-11-07 01:03:15 +0200

    9fgf

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     len(my_list)
-    new = my_list.replace(4, 9)
 
     if idx < 0 or idx >= len(my_list):
         return my_list
 
     else:
-        return new
+        my_list[idx] = element
+        return my_list
