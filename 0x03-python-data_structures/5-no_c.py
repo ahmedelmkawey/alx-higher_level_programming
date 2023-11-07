8ac30731fc750222802ab281bc2fae9f840f84df
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-11-07 02:35:36 +0200

    dgrgf

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace("c", " ")
-    return new_string
+    new = ""
+    for a in my_string:
+        if a != 'c' and a != 'C':
+            new += a
+    return new
