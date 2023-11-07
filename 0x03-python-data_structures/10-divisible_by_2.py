b3896f6fc7005ebc7e8372c4cd86fa194d79684f
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-11-07 04:39:26 +0200

    drfrfref

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+
     new_list = []
-    for number in range(len(my_list)):
-        if my_list[index] % 2 == 0:
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
-    return(new_list)
+
+    return (new_list)
