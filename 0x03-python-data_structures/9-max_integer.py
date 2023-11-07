359e3e403f9087fedbb7d093d4163dbe7d1507bb
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-11-07 02:47:49 +0200

    dgregrrgf

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+
+    if my_list = 0
+    return None
+
+    sort_list = my_list.sort()
+    max_value = sort_list[-1]
+    print("Max: {}".format(max_value))
