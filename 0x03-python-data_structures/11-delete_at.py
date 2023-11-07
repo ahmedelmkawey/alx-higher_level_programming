89d65131bf81dfb6faf7b7523056804384c8f1b2
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-11-07 03:01:47 +0200

    dergegrergf

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    lenght = len(my_list) - 1
+
+    if idx < 0 or idx > lenght:
+        return my_list
+
+    else:
+        new_list = del my_list[idx]
+        # new_list[idx] = element
+        return (new_list)
