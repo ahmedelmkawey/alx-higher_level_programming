4e168082fb6bd11ac1411b53e1678a9f15b9eca8
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-11-23 04:25:59 +0200

    gtfffl

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,9 @@
 class Square:
     """ Class Square """
     def __init__(self, size=0):
-        """ Constructor """
+        """
+        Constructor
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
