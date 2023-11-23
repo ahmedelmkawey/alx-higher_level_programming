363d50b5f80dea3f3f2ddd18526d788a9e667ab0
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-11-23 04:27:16 +0200

    gtffl

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" Class Square that defines a square """
+"""
+Class Square that defines a square
+"""
 
 
 class Square:
-    """ Class Square """
+    """
+    Class Square
+    """
     def __init__(self, size=0):
         """
         Constructor
