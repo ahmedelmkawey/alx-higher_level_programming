f6706e4e4714470b477b1f871a780b40efe5d2ab
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-12-06 04:40:44 +0200

    effddlfl

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
 """
-Class Module
+This module creates a student class
+with public attributes and retrives
+json dictionary rep
 """
 
 
 class Student:
     """
-    Student class
+    student class with public instances
     """
-
     def __init__(self, first_name, last_name, age):
-        """initialize method
+        """
+        instantiation of attr
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of student instance
-        args:
-            attrs: attributes
-        return:
-            dictionary
         """
-        if not attrs:
-            return self.__dict__
-
+        function returns dict repres
+        of instance
+        """
+        if attrs is None:
+            return (self.__dict__)
         return ({key: value for key, value in self.__dict__.items()
                  if key in attrs})
